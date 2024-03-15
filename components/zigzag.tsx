25e457d51443980e65c51ff9cd66f0f865190714
Brian Lalli <brianlalli@Brians-MacBook-Pro.local>
2024-03-14 20:09:36 -0400

    added about section to landing page

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -1,63 +1,117 @@
 import Image from "next/image";
+import FeatImage01 from "../public/images/hero-image.png";
 import FeatImage02 from "../public/images/cropped-diverse-network.png";
+import "../app/css/additional-styles/zigzag.css";
 
 export default function Zigzag() {
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="py-12 md:py-20">
-          {/* Section header for the 2nd item */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h2
-              className="text-3xl font-bold mb-6 mx-auto text-center"
-              style={{ color: "#435373" /* Assuming deep-slate is #435373 */ }}
-            >
-              Diverse Practitioner Network
-            </h2>
+        <div className="py-12 md:py-6">
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-10 pt-6">
+            <div className="inline-flex text-base font-semibold py-2 px-4 m-2 text-green-600 bg-green-200 rounded-full mb-4">
+              How It Works
+            </div>
           </div>
 
-          {/* 2nd item */}
-          <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
-            {/* Image */}
-            <div
-              className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
-              data-aos="fade-up"
-            >
-              <Image
-                className="max-w-full mx-auto md:max-w-none h-auto"
-                src={FeatImage02}
-                width={540}
-                height={405}
-                alt="Features 02"
-              />
-            </div>
-            {/* Content */}
-            <div
-              className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
-              data-aos="fade-left"
-            >
-              <div className="md:pl-4 lg:pl-12 xl:pl-16">
-                <div
-                  className="text-xl mb-2"
-                  style={{ color: "#435373" /* deep-slate */ }}
-                >
-                  More options. Easy access.
+          {/* Items */}
+          <div className="grid gap-20">
+            {/* 1st item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage01}
+                  width={540}
+                  height={405}
+                  alt="Features 01"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-right"
+              >
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">
+                  Personalized recommendations
+                  </div>
+                  <h3 className="h3 mb-3">
+                  A simple triage for tailored care
+                  </h3>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>
+                      Tell us a little about yourself so we can offer a recommended provider
+                      </span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>
+                      Select the right path for you - whether that’s meeting with a specialized practitioner or engaging in library of resources that can help guide you on a path to a healthier you
+                      </span>
+                    </li>
+                  </ul>
                 </div>
-                <h3 className="h3 mb-3" style={{ color: "#435373" }}>
-                  Access a diverse network of practitioners that help people
-                  find alternative and cutting-edge therapies to achieve a
-                  healthier and more fulfilling life.
-                </h3>
-                <p
-                  className="text-xl mb-4"
-                  style={{ color: "#9fa4b7" /* medium-slate */ }}
-                >
-                  Natu’s responsive approach to care supports people across a
-                  wide range of needs, interests and goals. Our personalized
-                  options are backed up by a practitioner network with varying
-                  clinical expertise and modalities to help make access to care
-                  easier than ever.
-                </p>
+              </div>
+            </div>
+
+            <div className="grid gap-10"></div>
+            {/* 2nd item */}
+            <div className="max-w-3xl mx-auto text-center">
+              <div className="inline-flex text-base font-semibold py-2 px-4 text-green-600 bg-green-200 rounded-full">
+                Diverse Practitioner Network
+              </div>
+            </div>
+
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage02}
+                  width={540}
+                  height={405}
+                  alt="Features 02"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-left"
+              >
+                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">
+                    More options. Easy access.
+                  </div>
+                  <h3 className="h3 mb-3">
+                  Access a diverse network of practitioners helps people find alternative and cutting-edge therapies to achieve a healthier and more fulfilling life{" "}
+                  </h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                  Natu’s responsive approach to care supports people across a wide range of needs, interests and goals. Our personalized options are backed up by a practitioner network with varying clinical expertise and modalities to help make access to care easier than ever.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
